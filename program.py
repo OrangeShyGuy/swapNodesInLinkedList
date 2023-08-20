cff98181bd83e791125ad8188a2b8a173f468442
OrangeShyGuy <62937288+OrangeShyGuy@users.noreply.github.com>
2023-08-21 01:59:20 +0200

    Create program.py
    finished version of the program

---
+++ program.py
@@ -0,0 +1,26 @@
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next         
+class Solution:
+     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+         if not head:
+           return head
+         dummyNode = ListNode()
+         previousNode = dummyNode
+         currentNode = head
+         if head.next == None:
+           return head
+         else:
+           adjacentNode = currentNode.next
+           while currentNode != None and currentNode.next != None:
+             #previousNode.next = currentNode
+             temp = adjacentNode.next
+             previousNode.next = currentNode.next
+             adjacentNode.next = currentNode
+             currentNode.next = temp
+             if temp != None:
+                 previousNode = currentNode
+                 currentNode = previousNode.next
+                 adjacentNode = currentNode.next
+         return dummyNode.next
